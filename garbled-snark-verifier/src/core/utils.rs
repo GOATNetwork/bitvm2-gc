d6a0e94f26de67196dc77bf5d95e8912d067a6b1
vanhger <vietanhpg2003@gmail.com>
2025-07-20 09:47:46 +0700

    feat: replace blake3 by poseidon2

--- garbled-snark-verifier/src/core/utils.rs
+++ garbled-snark-verifier/src/core/utils.rs
@@ -1,3 +1,17 @@
+use p3_field::{FieldAlgebra, PrimeField32};
+use p3_koala_bear::KoalaBear;
+
 pub fn bit_to_usize(bit: bool) -> usize {
     if bit { 1 } else { 0 }
+}
+
+pub fn poseidon2_hash(input: &[u8]) -> [u8; 32] {
+    let koalabear_input =
+        input.iter().map(|&b| KoalaBear::from_canonical_u8(b)).collect::<Vec<_>>();
+    let hash = zkm_primitives::poseidon2_hash(koalabear_input);
+    let mut output = [0u8; 32];
+    for (i, x) in hash.iter().enumerate() {
+        output[i * 4..(i + 1) * 4].copy_from_slice(&x.as_canonical_u32().to_le_bytes());
+    }
+    output
 }
